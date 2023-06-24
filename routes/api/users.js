ad865d29ca202a06ce1f3e4b2702b4e30310b5ba
amit-chaman <amitchaman369@gmail.com>
2023-06-24 15:40:49 +0530

    changes added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,56 @@
 const express = require('express');
 const router = express.Router();
-
+const gravatar = require('gravatar');
+const { check , validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
 // @ route  POST api/users
 // @ desc   Register User 
-// @ access Public
-router.get('/', (req,res) => {
-    console.log(req.body);
+// @ access Public 
+router.post('/', [ check('name' , 'Name is required').not().isEmpty(), check('email','Please include a valid email').isEmail(), check('password','Please enter password with 6 or more characters').isLength({ min : 6})] ,  async (req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty())
+    {
+        return res.status(400).json({ errors : errors.array()});
+    }
+    console.log(req.body);  
+
+    const { name, email , password } = req.body;
+  
+    try {
+    // see if user exists
+     let user = await User.findOne({ email});
+     if(user)
+     {
+        res.status(400).json({ errors :  [ { msg : 'User already exists'}]});
+     }
+
+    // get user gravatar 
+     const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+     })
+     user = new User({
+        name, 
+        email,
+        avatar,
+        password
+     })
+    // Encrypt password 
+
+
+    // Return jsonwebtoken 
+
     res.send('User route');
+    }
+    catch(err) 
+    {
+       console.error(err.message);
+       res.status(500).send('Server error');
+    }
+
+   
 }) 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 5000;
 // connect Database 
 connectDB();
 
+// init middleware
+app.use(express.json({ extended : false  }));
+
 // Define routes
 app.use('/api/users' , require('./routes/api/users'));
 app.use('/api/auth' , require('./routes/api/auth'));
